2ef88b2b4bdcdd1fa23b1ec0472bc2b6b51becc3
조준명 <a8456452@naver.com>
2020-04-05 23:45:53 +0900

    초기 설정 중 단계

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', Wel : 'Welcome to ' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('index', { title: 'TEST PAGE - PM2', Wel : 'This is ' });
 });
 
 module.exports = router;
