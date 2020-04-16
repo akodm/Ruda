1a166747b2f7375ac7a154889a9b45c46a821606
koyurii98 <rmeodprp0856@naver.com>
2020-04-16 22:40:25 +0900

    수정

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -3,6 +3,24 @@ import './layout.css';
 
 export default function Footer() {
     return <div className="footerMain">
-        Footer
+        <div className="footer_Content">
+       <div className="footer_logo">
+           <img src=""></img>
+       </div>
+       <div className="footer_text">
+            <p>
+                <span>경기도 안양시 동안구 비산동 임곡로 29 대림대학교</span>
+            </p>
+            <p>
+                <span>이메일 </span><span>ruda@ruda.com</span>
+            </p>
+            <p>
+                <span><a href="#">고객센터</a></span> |
+                <span><a href="#">광고문의</a></span> |
+                <span><a href="#">문의하기</a></span>
+            </p>
+           
+            </div>
+       </div>
     </div>
 };
