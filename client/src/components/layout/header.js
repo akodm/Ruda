ad95f73b048aa1c96d67237ac61b54687cc0e251
조준명 <a8456452@naver.com>
2020-04-19 00:14:20 +0900

    병합 후 파일 수정

--- client/src/components/layout/header.js
+++
@@ -1,9 +0,0 @@
-import React, { userState } from 'react';
-import { Link } from 'react-router-dom';
-import './layout.css';
-
-export default function Header() {
-    return <div className="headerMain">
-
-    </div>
-};
