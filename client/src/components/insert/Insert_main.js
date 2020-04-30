475e66899724d30919e3c632a27d2be3ebad976c
koyurii98 <rmeodprp0856@naver.com>
2020-04-30 17:18:03 +0900

    directory setting

---
+++ client/src/components/insert/Insert_main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Insert_main extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Insert_main;
---
+++ client/src/components/insert/Insert_rookie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Insert_rookie extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Insert_rookie;
