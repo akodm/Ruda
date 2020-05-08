239bb506e69285dda57f8a6a83625b71489b95aa
조준명 <a8456452@naver.com>
2020-05-08 23:40:47 +0900

    cors 오류 해결 및 테스트 코드 제거

--- client/src/components/mypopup/MyPopup.js
+++ client/src/components/mypopup/MyPopup.js
@@ -14,14 +14,7 @@ class MyPopup extends Component {
             alrDisplay : "none",
         }
     }
-    async testMail() {
-        try {
-            await axios.get("http://localhost:5000/users/all");
-            console.log("노드 메일러 실행");
-        } catch (err) {
-            console.log("에러 발생 : " + err)
-        }
-    }
+
     render() {
         const { msgDisplay,alrDisplay } = this.state;
         return (
@@ -31,7 +24,7 @@ class MyPopup extends Component {
                 <div className="mypopup-div">
                     <Link to="/"><img src="/Image/menu_home.png" alt="MENUIMG"></img></Link>
                     <img onClick={() => this.setState({ alrDisplay : alrDisplay === "none" ? "flex" : "none"})} className="mypopup-menuImg" src="/Image/menualram.png" alt="MENUIMG"></img>
-                    <img onClick={this.testMail.bind(this)} className="mypopup-menuImg" src="/Image/menu_agreement.png" alt="MENUIMG"></img>
+                    <img className="mypopup-menuImg" src="/Image/menu_agreement.png" alt="MENUIMG"></img>
                     <img onClick={() => this.setState({ msgDisplay : msgDisplay === "none" ? "flex" : "none"})} className="mypopup-menuImg" src="/Image/menu_mail.png" alt="MENUIMG"></img>
                 </div>
             </div>
