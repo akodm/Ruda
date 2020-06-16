e89678e61a8b5d219bc34d56f8a813088d476b11
조준명 <a8456452@naver.com>
2020-06-17 02:29:54 +0900

    게시판의 카드 컴포넌트 에러 해결 및 데이터 뿌리기

--- client/src/component/Rookie/RookieCard.js
+++ client/src/component/Rookie/RookieCard.js
@@ -1,37 +1,39 @@
 import React, { Component } from 'react';
-import './Rookie.css';
 
-export default function RookieCard(props) {
-    let userList = props.userList;
-    console.log(userList);
-    return (
-        <div className="Rookie-Card">
-            <div className="Rookie-Card-header">
-                <div className="Rookie-Card-like">
-                    <img src="/Images/1216649.svg" width="12px"height="12px" alt="img"/>
-                    <span>14</span>
+class RookieCard extends Component {
+    render() {
+        const userList = this.props.userList;
+        return userList ? (
+            <div className="Rookie-Card">
+                <div className="Rookie-Card-header">
+                    <div className="Rookie-Card-like">
+                        <img src="/Images/1216649.svg" width="12px"height="12px" alt="img"/>
+                        <span>{userList.userLike}</span>
+                    </div>
+                    <div className="Rookie-Card-state">
+                        <div className="Rookie-search-title-state-training"></div>
+                        <div className="Rookie-search-title-state-hire"></div>
+                    </div>
                 </div>
-                <div className="Rookie-Card-state">
-                    <div className="Rookie-search-title-state-training"></div>
-                    <div className="Rookie-search-title-state-hire"></div>
-                </div>
-            </div>
-            <div className="Rookie-Card-Profile">
-                <div className="Rookie-Card-Profile-img" >
-                    <img src="/Images/easy_icon.png" alt="img"/>
-                </div>
-                <div className="Rookie-Card-Profile-info">
-                    <span className="Rookie-Card-Profile-info-name">{}</span>
-                    <span className="Rookie-Card-Profile-info-text">{}</span>
-                    <span className="Rookie-Card-Profile-info-position">{}</span>
-                    <span className="Rookie-Card-Profile-info-pt">포트폴리오 14개</span>
-                    <div className="Rookie-Card-Profile-info-tags">
-                        <span className="Rookie-Card-Profile-info-tags-tag">#JAVA</span>
-                        <span className="Rookie-Card-Profile-info-tags-tag">#PHP</span>
-                        <span className="Rookie-Card-Profile-info-tags-tag">#Node.js</span>
+                <div className="Rookie-Card-Profile">
+                    <div className="Rookie-Card-Profile-img" >
+                        <img src={userList.userImageUrl || "Images/footer_logo.png"} alt="img"/>
+                    </div>
+                    <div className="Rookie-Card-Profile-info">
+                        <span className="Rookie-Card-Profile-info-name">{userList.userName}</span>
+                        <span className="Rookie-Card-Profile-info-text">{userList.userIntro}</span>
+                        <span className="Rookie-Card-Profile-info-position">{userList.userField || "미정"}</span>
+                        <span className="Rookie-Card-Profile-info-pt">포트폴리오 14개</span>
+                        <div className="Rookie-Card-Profile-info-tags">
+                            { userList.userTags.map((data,i) => {
+                                return <span key={i} className="Rookie-Card-Profile-info-tags-tag">{data}</span>
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        ) : <div></div>
+    }
 }
+
+export default RookieCard;
